bd4fb82bb4e5a7a59976083ec74349b171c80b90
Lucas Goodding <gooddinglucas@gmail.com>
2019-03-19 12:37:22 -0500

    experimenting w/ arrays

---
+++ lucas/fruits.py
@@ -0,0 +1,10 @@
+import xarray
+
+list = ["apple", "bannana", "cherry"]
+
+for index in list:
+    print(index)
+    if index == "apple":
+        print("it is an apple")
+    else:
+        print("it is not an apple")
