f9f625d584014782cf08ae6228e15574a682f12e
Lucas Goodding <gooddinglucas@gmail.com>
2019-05-11 13:44:17 -0500

    loops

--- lucas/loop.py
+++ lucas/loop.py
@@ -1,8 +1,13 @@
+# determine size of list
+
 size = int(input("What size of list do you want? "))
-
-list = []
-
+# Populate list
 for index in range(size):
     index = int(input("what do you want in index %s ? " %index))
 
-for index in range(size): print(index)
+print("Here is the arrray:")
+# for index in range(size): print(index)
+
+# Print conents of list
+for index in range(size):
+    print()
