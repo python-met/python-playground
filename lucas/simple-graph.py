f95cbefd9811daf88c1ad47873264258605921b0
Lucas Goodding <gooddinglucas@gmail.com>
2019-06-12 16:34:03 -0500

    new graphing

--- lucas/simple-graph.py
+++ lucas/simple-graph.py
@@ -4,8 +4,8 @@
 import matplotlib.pyplot as plt
 
 # define x and y with lists
-x = [1,2,3]
-y = [1,4,9]
+x = [1,2,3,4,5,6,7,8,9]
+y = [1,4,9,9,5,4,3,4,6]
 
 x2 = [1,5,6]
 y2 = [2,4,4]
@@ -15,8 +15,8 @@ plt.plot(x, y, label='1st series')
 plt.plot(x2, y2, label='2nd series')
 
 # set labels
-plt.xlabel('x label')
-plt.ylabel('y label')
+plt.xlabel('time')
+plt.ylabel('pressure')
 
 #label axis, title
 plt.xlabel('x label')
